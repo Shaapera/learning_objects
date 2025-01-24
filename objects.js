d497491652cb765093ccd283d067d912f1bcfdc8
Shaapera <shalomshaapera@gmail.com>
2025-01-24 02:01:46 +0100

    basic object methods

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-const Shaapera = {
+/*const Shaapera = {
     firstName: 'Shalom',
     lastName: 'Shaapera',
     age: 2037 - 1998,
@@ -29,4 +29,26 @@ console.log(Shaapera);
 
 console.log(`${Shaapera.firstName} has ${Shaapera.friends.length} friends, and her best friend is called ${Shaapera.friends[1]}`);
 
+//object methods*/
+
 //object methods
+const Shaapera = {
+    firstName: 'Shalom',
+    lastName: 'Shaapera',
+    birthyear: 1998,
+    friends: ['Emeka', 'Kuramter'],
+    hasDriversLicense: true,
+   // calcAge: function (birthyear) {
+     //   return 2037 - birthyear;
+
+    //}
+    calcAge: function () {
+        this.age = 2037 - this.birthday;
+        console.log(this);
+        return this.age;
+    }
+};
+
+console.log(Shaapera.calcAge());
+//console.log(Shaapera.calcAge(1897));
+//console.log(Shaapera['calcAge'](1897));
